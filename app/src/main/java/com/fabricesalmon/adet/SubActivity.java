a50df8a9cea588e333178e0e285653a3cae649fc
fsjm <mail@fabricesalmon.com>
2015-11-15 15:59:01 +0100

    A test

--- app/src/main/java/com/fabricesalmon/adet/SubActivity.java
+++ app/src/main/java/com/fabricesalmon/adet/SubActivity.java
@@ -2,12 +2,91 @@ package com.fabricesalmon.adet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SubActivity extends AppCompatActivity {
+    private final String ms_TAG = this.getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(ms_TAG, "On Start .....");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        Log.i(ms_TAG, "On Restart .....");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i(ms_TAG, "On Resume .....");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle l_Bundle) {
+        super.onSaveInstanceState(l_Bundle);
+
+        // Save UI state changes to the savedInstanceState.
+        // This bundle will be passed to onCreate if the process is
+        // killed and restarted.
+
+        Log.i(ms_TAG, "On SaveInstanceState .....");
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle l_Bundle) {
+        super.onRestoreInstanceState(l_Bundle);
+
+        // Restore UI state from the savedInstanceState.
+        // This bundle has also been passed to onCreate.
+
+        Log.i(ms_TAG, "On RestoreInstanceState .....");
+    }
+
+    private void RestoreInstanceState(Bundle l_Bundle) {
+
+        if (null == l_Bundle) return;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i(ms_TAG, "On Pause .....");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i(ms_TAG, "On Stop .....");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i(ms_TAG, "On Destroy .....");
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        Log.i(ms_TAG, "On BackPressed .....");
+
+        // Otherwise defer to system default behavior.
+        super.onBackPressed();
     }
 }
