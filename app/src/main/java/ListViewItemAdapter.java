3af3e2fee9ca58f34fcb8de87ee7e082b20a8da2
fsjm <mail@fabricesalmon.com>
2015-11-25 14:33:11 +0100

    A test

--- app/src/main/java/ListViewItemAdapter.java
+++ app/src/main/java/ListViewItemAdapter.java
@@ -27,8 +27,8 @@ public class ListViewItemAdapter extends ArrayAdapter<ListViewItem> {
         ListViewItemHolder l_ListViewItemHolder = (ListViewItemHolder) convertView.getTag();
         if(l_ListViewItemHolder == null){
             l_ListViewItemHolder = new ListViewItemHolder();
-            l_ListViewItemHolder.m_Pseudo = (TextView) convertView.findViewById(R.id.pseudo);
-            l_ListViewItemHolder.m_Avatar = (ImageView) convertView.findViewById(R.id.avatar);
+            l_ListViewItemHolder.setPseudo((TextView) convertView.findViewById(R.id.pseudo));
+            l_ListViewItemHolder.setAvatar((ImageView) convertView.findViewById(R.id.avatar));
             convertView.setTag(l_ListViewItemHolder);
         }
 
@@ -36,15 +36,28 @@ public class ListViewItemAdapter extends ArrayAdapter<ListViewItem> {
         ListViewItem l_ListViewItem = getItem(position);
 
         //il ne reste plus qu'à remplir notre vue
-        l_ListViewItemHolder.m_Pseudo.setText(l_ListViewItem.getPseudo());
-        l_ListViewItemHolder.m_Avatar.setImageDrawable(new ColorDrawable(l_ListViewItem.getColor()));
+        l_ListViewItemHolder.getPseudo().setText(l_ListViewItem.getPseudo());
+        l_ListViewItemHolder.getAvatar().setImageDrawable(new ColorDrawable(l_ListViewItem.getColor()));
 
         return convertView;
     }
 
     private class ListViewItemHolder{
-        public TextView m_Pseudo;
-        public ImageView m_Avatar;
+        private TextView m_Pseudo;
+        private ImageView m_Avatar;
+
+        public TextView getPseudo() {
+            return m_Pseudo;
+        }
+        public void setPseudo(TextView l_Pseudo) {
+            m_Pseudo = l_Pseudo;
+        }
+        public ImageView getAvatar() {
+            return m_Avatar;
+        }
+        public void setAvatar(ImageView l_Avatar) {
+            m_Avatar = l_Avatar;
+        }
     }
 }
 
